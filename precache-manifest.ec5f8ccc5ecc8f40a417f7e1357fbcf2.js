273321562cbc5433e4f139bc134c43eb46834a5a
clintonpi <nkwochaclinton@gmail.com>
2021-10-19 06:18:03 +0100

    Updates

---
+++ precache-manifest.ec5f8ccc5ecc8f40a417f7e1357fbcf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982ba0550c267366258667a364bb1c5c",
+    "url": "/sw-client/index.html"
+  },
+  {
+    "revision": "e52312c1b081917eb23e",
+    "url": "/sw-client/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e52312c1b081917eb23e",
+    "url": "/sw-client/static/js/2.727be1fa.chunk.js"
+  },
+  {
+    "revision": "9e3f283d2267c3b4c073",
+    "url": "/sw-client/static/js/main.d1d75af9.chunk.js"
+  },
+  {
+    "revision": "56250c0b6af68fdcac02",
+    "url": "/sw-client/static/js/runtime~main.1f64aa1e.js"
+  }
+]);
